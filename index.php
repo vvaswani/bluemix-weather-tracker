a42b991e9ec958e10a60b5d085aa0556ac7f2ae1
vvaswani <vvaswani@users.noreply.github.com>
2015-12-10 16:17:56 +0530

    Added buildpack options file. Updated views and added VCAP_SERVICES variable for Bluemix.

--- index.php
+++ index.php
@@ -27,25 +27,32 @@ $dbn = substr(parse_url($app->config['db_uri'], PHP_URL_PATH), 1);
 $mongo = new MongoClient($app->config['db_uri'], array("connectTimeoutMS" => 30000));
 $db = $mongo->selectDb($dbn);
 
+// if BlueMix VCAP_SERVICES environment available
+// overwrite with credentials from BlueMix
+if ($services = getenv("VCAP_SERVICES")) {
+  $services_json = json_decode($services, true);
+  $app->config['weather_uri'] = $services_json["weatherinsights"][0]["credentials"]["url"];
+} 
+
 // index page handlers
 $app->get('/', function () use ($app) {
   return $app->redirect($app["url_generator"]->generate('index'));
 });
 
 $app->get('/index', function () use ($app, $db) {
-  // get list of cities from database
-  // for each city, get current weather from Weather service
-  $collection = $db->cities;  
-  $cities = iterator_to_array($collection->find());
-  foreach ($cities as &$city) {
-    $uri = $app->config['weather_uri'] . '/api/weather/v2/observations/current?units=m&language=en-US&geocode=' . urlencode(sprintf('%f,%f', $city['lat'], $city['lng']));
+  // get list of locations from database
+  // for each location, get current weather from Weather service
+  $collection = $db->locations;  
+  $locations = iterator_to_array($collection->find());
+  foreach ($locations as &$location) {
+    $uri = $app->config['weather_uri'] . '/api/weather/v2/observations/current?units=m&language=en-US&geocode=' . urlencode(sprintf('%f,%f', $location['lat'], $location['lng']));
     $json = file_get_contents($uri);
     if ($json === FALSE) {
      throw new Exception("Could not connect to Weather API.");  
     }
-    $city['weather'] = json_decode($json, true);
+    $location['weather'] = json_decode($json, true);
   }
-  return $app['twig']->render('index.twig', array('cities' => $cities));
+  return $app['twig']->render('index.twig', array('locations' => $locations));
 })
 ->bind('index');
 
@@ -57,8 +64,8 @@ $app->get('/search', function () use ($app) {
 
 // search processor
 $app->post('/search', function (Request $request) use ($app) {
-  // search for city string against Geonames database
-  // for each result, store city name, country and Geonames ID
+  // search for location string against Geonames database
+  // for each result, store location name, country and Geonames ID
   $query = urlencode(strip_tags($request->get('query')));
   $sxml = simplexml_load_file('http://api.geonames.org/search?q=' . $query . '&maxRows=20&username=' . $app->config['geonames_uid']);
   if ($sxml === FALSE) {
@@ -68,66 +75,66 @@ $app->post('/search', function (Request $request) use ($app) {
   foreach ($sxml->geoname as $g) {
     $data[] = array(
       'gid' => (int)$g->geonameId,
-      'city' => (string)$g->name,
+      'location' => (string)$g->name,
       'country' => (string)$g->countryName,
     );
   }
-  return $app['twig']->render('search.twig', array('data' => $data, 'query' => $query));
+  return $app['twig']->render('search.twig', array('data' => $data, 'query' => urldecode($query)));
 });
 
-// handler to add city to database
+// handler to add location to database
 $app->get('/add/{gid}', function ($gid) use ($app, $db) {
-  // use Geonames ID to get city latitude/longitude from Geonames service
+  // use Geonames ID to get location latitude/longitude from Geonames service
   // connect to MongoDB and save in database
-  $collection = $db->cities;
+  $collection = $db->locations;
   $query = (int)urlencode(strip_tags($gid));
   $sxml = simplexml_load_file('http://api.geonames.org/get?geonameId=' . $query . '&username=' . $app->config['geonames_uid']);
   if ($sxml === FALSE) {
     throw new Exception("Could not connect to Geonames API.");  
   }
-  $city = new stdClass;
-  $city->gid = trim(strip_tags($sxml->geonameId));
-  $city->city = trim(strip_tags($sxml->name));
-  $city->country = trim(strip_tags($sxml->countryCode));
-  $city->lat = trim(strip_tags($sxml->lat));
-  $city->lng = trim(strip_tags($sxml->lng));
+  $location = new stdClass;
+  $location->gid = trim(strip_tags($sxml->geonameId));
+  $location->location = trim(strip_tags($sxml->name));
+  $location->country = trim(strip_tags($sxml->countryCode));
+  $location->lat = trim(strip_tags($sxml->lat));
+  $location->lng = trim(strip_tags($sxml->lng));
   $cursor = iterator_to_array($collection->find());
-  // disallow if 5 cities already exist
+  // disallow if 5 locations already exist
   if (count($cursor) >= 5) {
-    throw new Exception("A maximum of 5 cities are supported. Please remove a city and try again.");
+    throw new Exception("A maximum of 5 locations are supported. Please remove a location and try again.");
   }
-  // disallow if selected city already exists
+  // disallow if selected location already exists
   foreach ($cursor as $key => $value) {
-    if ($value['gid'] == $city->gid) {
-      throw new Exception("The selected city already exists in the city list.");  
+    if ($value['gid'] == $location->gid) {
+      throw new Exception("The selected location already exists in the location list.");  
     }
   }
-  $collection->save($city);
+  $collection->save($location);
   return $app->redirect($app["url_generator"]->generate('index'));
 })
 ->bind('add');
 
-// handler to remove city from database
+// handler to remove location from database
 $app->get('/delete/{id}', function ($id) use ($app, $db) {
-  $collection = $db->cities;
+  $collection = $db->locations;
   $collection->remove(array('_id' => new MongoId($id)));
   return $app->redirect($app["url_generator"]->generate('index'));
 })
 ->bind('delete');
 
-// handler to display 7-day forecast for selected city
+// handler to display 7-day forecast for selected location
 $app->get('/forecast/{id}', function ($id) use ($app, $db) {
-  // look up city record in database
+  // look up location record in database
   // connect and get forecast from Weather service
-  $collection = $db->cities;
-  $city = (array)$collection->findOne(array('_id' => new MongoId($id)));
-  $uri = $app->config['weather_uri'] . '/api/weather/v2/forecast/daily/10day?units=m&language=en-US&geocode=' . urlencode(sprintf('%f,%f', $city['lat'], $city['lng']));
+  $collection = $db->locations;
+  $location = (array)$collection->findOne(array('_id' => new MongoId($id)));
+  $uri = $app->config['weather_uri'] . '/api/weather/v2/forecast/daily/10day?units=m&language=en-US&geocode=' . urlencode(sprintf('%f,%f', $location['lat'], $location['lng']));
   $json = file_get_contents($uri);
   if ($json === FALSE) {
     throw new Exception("Could not connect to Weather API.");  
   }
-  $city['weather'] = json_decode($json, true);
-  return $app['twig']->render('forecast.twig', array('data' => $city));
+  $location['weather'] = json_decode($json, true);
+  return $app['twig']->render('forecast.twig', array('data' => $location));
 })
 ->bind('forecast');
 
